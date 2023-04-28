cf421ab5a4d040af5658acbe5a4790d9ff7a4c47
unknown <annieyashley@gmail.com>
2023-04-28 15:37:23 -0500

    adds comments to code, adds image of deployed page

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/api/notes', (req,res) =>
   })
 )
 
+// gets a specific note when clicked on in the sidebar
 app.get('/api/notes/:id', (req,res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     const dataParse = JSON.parse(data)
@@ -62,7 +63,7 @@ app.post('/api/notes', (req,res) => {
   }
 })
 
-// get notes
+// wildcard route
 app.get('*', (req,res) =>
     res.sendFile(path.join(__dirname, "./public/index.html"))
 )
